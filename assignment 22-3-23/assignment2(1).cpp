cd3f65b633d7dd9cc292a9b8d775ebf9c9eef12f
suthesan vj <118433249+suthesanvj@users.noreply.github.com>
2023-03-23 14:09:50 +0530

    Add files via upload

---
+++ assignment 22-3-23/assignment2(1).cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+	int result = ((1+2)*3+4)*5;
+	printf("result=%d\n",result);
+	return 0;
+}
---
+++ assignment 22-3-23/assignment2(2).cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	
+const int a = 10;
+printf("%d",  a);
+return 0;
+}
+
